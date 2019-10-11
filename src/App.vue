61665b0495563b9278bf9ee43019d1b7057183e8
Nikita Jiandani <niki.jiandani@gmail.com>
2019-10-11 13:36:51 -0400

    Add my Component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <NikiJiandani msg="Hello from Niki's component!" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import NikiJiandani from "./components/NikiJiandani.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    NikiJiandani
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/NikiJiandani.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: "NikiJiandani",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+div {
+  font-size: 2rem;
+  color: aquamarine;
+}
+</style>
