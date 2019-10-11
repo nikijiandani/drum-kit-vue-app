b4b783906950c2267fa99d73b993de8ed2639b3f
perrymacdougall <perrymacdougall@gmail.com>
2019-10-11 16:01:00 -0400

    mapped different sounds to keys along the top row

--- src/components/PerrysComponent.vue
+++ src/components/PerrysComponent.vue
@@ -6,11 +6,83 @@
 </template>
 
 <script>
+let sounds = {
+    q: require('../sounds/boom.wav'),
+    w: require('../sounds/clap.wav'),
+    e: require('../sounds/hihat.wav'),
+    r: require('../sounds/kick.wav'),
+    t: require('../sounds/openhat.wav'),
+    y: require('../sounds/ride.wav'),
+    u: require('../sounds/snare.wav'),
+    i: require('../sounds/tink.wav'),
+    o: require('../sounds/tom.wav')
+}
+
+
+
 export default {
   name: 'PerrysComponent',
   props: {
     msg: String
-  }
+  },
+  methods: {
+    playSound: (e) => {
+        switch (e.keyCode) {
+            case 81:
+                let audioQ = new Audio(sounds.q);
+                audioQ.play();
+                break;
+            case 87:
+                let audioW = new Audio(sounds.w);
+                audioW.play();
+                break;
+            case 69:
+                let audioE = new Audio(sounds.e);
+                audioE.play();
+                break;
+            case 82:
+                let audioR = new Audio(sounds.r);
+                audioR.play();
+                break;
+            case 84:
+                let audioT = new Audio(sounds.t);
+                audioT.play();
+                break;
+            case 89:
+                let audioY = new Audio(sounds.y);
+                audioY.play();
+                break;
+            case 85:
+                let audioU = new Audio(sounds.u);
+                audioU.play();
+                break;
+            case 73:
+                let audioI = new Audio(sounds.i);
+                audioI.play();
+                break;
+            case 79:
+                let audioO = new Audio(sounds.o);
+                audioO.play();
+                break;
+        }
+    },
+    // onKeyDown: (e) {
+    //     switch (e.keyCode) {
+    //         case 81: 
+    //             this.playSound(sound1);
+    //             break;
+    //         default:
+    //             console.log('error')
+    //             break;
+    //     }
+    // }
+  },
+  created: function() {
+      window.addEventListener('keydown', this.playSound);
+  },
+  destroyed: function() {
+      window.removeEventListener('keydown', this.playSound);
+  },
 }
 </script>
 
@@ -31,3 +103,6 @@ a {
   color: #42b983;
 }
 </style>
+
+
+
